7bb5f9e9555b176f64a6d50c0420d6a868cb8fc1
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-17 16:02:34 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// Remember to change deploy-commands back to gloabl scope
+const Discord = require("discord.js")
+require("dotenv").config()
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(process.env.PORT || 3500)
+
+const fs = require('fs')
+const path = require('path')
+const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.MessageContent] })
+app.client = client
+
+client.commands = new Discord.Collection()
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`⚠️ | The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
+require("./modules/deploy-commands")()
+
+client.login(process.env.TOKEN)
